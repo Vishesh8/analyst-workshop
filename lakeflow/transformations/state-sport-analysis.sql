2e5b77311a935d948f86c804211c5b865997eb3b
visheshh.arya87@gmail.com <visheshh.arya87@gmail.com>
2025-07-01 06:52:47 +0000

    updates

---
+++ lakeflow/transformations/state-sport-analysis.sql
@@ -0,0 +1,10 @@
+USE CATALOG `dbdemos_vishesh`;
+USE SCHEMA `contest_mock`;
+
+CREATE OR REPLACE MATERIALIZED VIEW `wallet_tx_agg` AS
+select c.contest_sport, u.origin_state, count(distinct u.user_id) as user_count
+from users u
+left join contests_tx ct on u.user_id = ct.user_id
+left join contests c on ct.contest_id = c.contest_id
+group by all
+order by all
---
+++ lakeflow/transformations/wallet-tx-agg.sql
@@ -0,0 +1,9 @@
+USE CATALOG `dbdemos_vishesh`;
+USE SCHEMA `contest_mock`;
+
+CREATE OR REPLACE MATERIALIZED VIEW `wallet_tx_agg` AS
+select year(tx_end_timestamp) as year, instrument_type, instrument,
+sum(amount) as amount, sum(discount_points) as discount_points, sum(net_amount) as net_amount
+from wallet_tx
+group by all
+order by all;
